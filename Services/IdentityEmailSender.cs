b587421db96aae36d9e8fa7e75304138eddae1d1
Lorenzo Tesler-Mabe <lorenzoteslermabe@gmail.com>
2026-01-27 11:34:04 -0800

    chore: implement identity IEmailSender with IdentityEmailSender.cs

---
+++ Services/IdentityEmailSender.cs
@@ -0,0 +1,23 @@
+using FluentEmail.Core;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace ssdp_2600.Services;
+
+public class IdentityEmailSender : IEmailSender
+{
+    private readonly IFluentEmail _email;
+
+    public IdentityEmailSender(IFluentEmail email)
+    {
+        _email = email;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        await _email
+            .To(email)
+            .Subject(subject)
+            .Body(htmlMessage)
+            .SendAsync();
+    }
+}
