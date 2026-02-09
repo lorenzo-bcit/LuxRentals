cac7a71eee625dccffb35f87ed0c3d26185f800c
Lorenzo Tesler-Mabe <lorenzoteslermabe@gmail.com>
2026-02-09 12:10:08 -0800

    feat: add repository interfaces for car data operations

---
+++ Repositories/Cars/ICarWriteRepository.cs
@@ -0,0 +1,13 @@
+using LuxRentals.Models;
+
+namespace LuxRentals.Repositories.Cars;
+
+public interface ICarWriteRepository
+{
+    public Task<Car?> GetByIdAsync(int id);
+    public Task AddAsync(Car car);
+    public Task SaveChangesAsync();
+
+    public Task<bool> VinExistsAsync(string vin, int? excludeCarId);
+    public Task<bool> PlateExistsAsync(string plate, int? excludeCarId);
+}
