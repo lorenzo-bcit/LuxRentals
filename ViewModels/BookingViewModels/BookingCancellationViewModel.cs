a09f9e7c4b5ca71300e5e1de1ad9310440bc84d3
Mohan Grewal <mohangrewal24@gmail.com>
2026-02-09 20:33:49 -0800

    feature: Booking Feature - Adjusted View Model Implementations

--- ViewModels/BookingViewModels/BookingCancellationViewModel.cs
+++ ViewModels/BookingViewModels/BookingCancellationViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace LuxRentals.ViewModels.BookingViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxRentals.ViewModels.BookingViewModels
 {
     public class BookingCancellationViewModel
     {
+
+        private int PkBookingId { get; set; }
+
+        // TODO: Add more details to the view model if needed, such as car details etc
+
+        [Display(Name = "Pickup Date")]
+        private DateTime StartDateTime { get; set; }
+
+        [Display(Name = "Return Date")]
+        private DateTime EndDateTime { get; set; }
+
+        [Display(Name = "Booking Status")]
+        private bool BookingStatus { get; set; }
+
+
+        public bool CanCancel { get; set; }
+
+        public string? Message { get; set; }
     }
 }
--- ViewModels/BookingViewModels/BookingCreateViewModel.cs
+++ ViewModels/BookingViewModels/BookingCreateViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace LuxRentals.ViewModels.BookingViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxRentals.ViewModels.BookingViewModels
 {
     public class BookingCreateViewModel
     {
+        [Required]
+        [Display(Name = "Pickup Date")]
+        private DateTime StartDateTime { get; set; }
+
+        [Required]
+        [Display(Name = "Return Date")]
+        private DateTime EndDateTime { get; set; }
+
+
     }
 }
