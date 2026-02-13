82c648d881b498c63db014fa59adf576c43ba698
Mohan Grewal <mohangrewal24@gmail.com>
2026-02-13 12:14:40 -0800

    feature: BookingsFeature - Updated Repo and Added BookingController

--- ViewModels/Bookings/BookingCancellationViewModel.cs
+++ ViewModels/Bookings/BookingCancellationViewModel.cs
@@ -5,18 +5,18 @@ namespace LuxRentals.ViewModels.BookingViewModels
     public class BookingCancellationViewModel
     {
 
-        private int PkBookingId { get; set; }
+        public int PkBookingId { get; set; }
 
         // TODO: Add more details to the view model if needed, such as car details etc
 
         [Display(Name = "Pickup Date")]
-        private DateTime StartDateTime { get; set; }
+        public DateTime StartDateTime { get; set; }
 
         [Display(Name = "Return Date")]
-        private DateTime EndDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
 
         [Display(Name = "Booking Status")]
-        private bool BookingStatus { get; set; }
+        public bool BookingStatus { get; set; }
 
 
         public bool CanCancel { get; set; }
