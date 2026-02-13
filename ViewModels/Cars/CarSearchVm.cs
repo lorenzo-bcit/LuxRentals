47f93a72218ac653be373222e693be9606528126
Lorenzo Tesler-Mabe <lorenzoteslermabe@gmail.com>
2026-02-12 21:19:05 -0800

    feat: enhance CarRepository search with availability date filtering and query refactoring

--- ViewModels/Cars/CarSearchVm.cs
+++ ViewModels/Cars/CarSearchVm.cs
@@ -10,4 +10,6 @@ public class CarSearchVm
     public bool AvailableOnly { get; set; } = true;
     public int Page { get; set; } = 1;
     public int PageSize { get; set; } = 10;
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
 }
