84ae2a7e342ee70ca19538e43113005928514b7f
Poul <poulisbister@gmail.com>
2026-02-20 11:00:58 -0800

    feat: added Payment model and added link to it in the transaction table

---
+++ Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿namespace LuxRentals.Models
+{
+    public class Payment
+    {
+        public int PkPaymentId { get; set; }
+        public int FkTransactionId { get; set; }
+        public Transaction Transaction { get; set; } = null!;
+        public string PaymentProvider { get; set; } = "PayPal";
+        public string PaymentProviderOrderId { get; set; } = null!;
+        public string PaymentProviderCaptureId { get; set; } = null!;
+        public PaymentStatus Status { get; set; }
+        public DateTime RecivedAt { get; set; }
+        public string? RawWebHookJson { get; set; }
+    }
+}
