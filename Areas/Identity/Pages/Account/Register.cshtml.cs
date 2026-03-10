8f4bf17fc6c5b5de1cf44e390a994c3008b63a02
Aman <173646085+mkdiraman@users.noreply.github.com>
2026-03-10 08:51:16 -0700

    chore: update namespace for servieces

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using LuxRentals.Services;
+using LuxRentals.Services.ServiceSettings;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
