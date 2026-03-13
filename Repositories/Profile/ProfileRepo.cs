d5a1261b9ad20682af3f5425ed331e70f2c336ed
samankayhanian <skayhanian@my.bcit.ca>
2026-03-13 15:44:55 -0700

    Feat : added details and edit to profile

--- Repositories/Profile/ProfileRepo.cs
+++ Repositories/Profile/ProfileRepo.cs
@@ -19,19 +19,19 @@ namespace LuxRentals.Repositories.Roles
         {
             try
             {
-                return await _db.Customers
+            return await _db.Customers
                     .Select(c => new ProfileVm
-                    {
-                        PkCustomerId = c.PkCustomerId,
-                        UserId = c.UserId,
-                        FirstName = c.FirstName,
-                        LastName = c.LastName,
-                        Email = c.Email,
-                        PhoneNumber = c.PhoneNumber,
-                        DriverLicenceNo = c.DriverLicenceNo
-                    })
-                    .ToListAsync();
-            }
+                {
+                    PkCustomerId = c.PkCustomerId,
+                    UserId = c.UserId,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Email = c.Email,
+                    PhoneNumber = c.PhoneNumber,
+                    DriverLicenceNo = c.DriverLicenceNo
+                })
+                .ToListAsync();
+        }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error retrieving all profiles");
@@ -43,20 +43,20 @@ namespace LuxRentals.Repositories.Roles
         {
             try
             {
-                return await _db.Customers
+            return await _db.Customers
                     .Where(c => c.PkCustomerId == id)
                     .Select(c => new ProfileVm
-                    {
-                        PkCustomerId = c.PkCustomerId,
-                        UserId = c.UserId,
-                        FirstName = c.FirstName,
-                        LastName = c.LastName,
-                        Email = c.Email,
-                        PhoneNumber = c.PhoneNumber,
-                        DriverLicenceNo = c.DriverLicenceNo
-                    })
-                    .FirstOrDefaultAsync();
-            }
+                {
+                    PkCustomerId = c.PkCustomerId,
+                    UserId = c.UserId,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Email = c.Email,
+                    PhoneNumber = c.PhoneNumber,
+                    DriverLicenceNo = c.DriverLicenceNo
+                })
+                .FirstOrDefaultAsync();
+        }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error retrieving profile for customer ID {CustomerId}", id);
@@ -67,21 +67,21 @@ namespace LuxRentals.Repositories.Roles
         public async Task<ProfileVm?> GetUserByEmailAsync(string email)
         {
             try
-            {
-                return await _db.Customers
+        {
+            return await _db.Customers
                     .Where(c => c.Email == email)
                     .Select(c => new ProfileVm
-                    {
-                        PkCustomerId = c.PkCustomerId,
-                        UserId = c.UserId,
-                        FirstName = c.FirstName,
-                        LastName = c.LastName,
-                        Email = c.Email,
-                        PhoneNumber = c.PhoneNumber,
-                        DriverLicenceNo = c.DriverLicenceNo
-                    })
-                    .FirstOrDefaultAsync();
-            }
+                {
+                    PkCustomerId = c.PkCustomerId,
+                    UserId = c.UserId,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Email = c.Email,
+                    PhoneNumber = c.PhoneNumber,
+                    DriverLicenceNo = c.DriverLicenceNo
+                })
+                .FirstOrDefaultAsync();
+        }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error retrieving profile for email {Email}", email);
@@ -92,27 +92,28 @@ namespace LuxRentals.Repositories.Roles
         public async Task<bool> UpdateProfileAsync(ProfileVm model)
         {
             try
-            {
-                var customer = await _db.Customers
-                    .FirstOrDefaultAsync(c => c.PkCustomerId == model.PkCustomerId);
+        {
+            var customer = await _db.Customers
+                .FirstOrDefaultAsync(c => c.PkCustomerId == model.PkCustomerId);
 
-                if (customer == null)
-                {
+            if (customer == null)
+            {
                     _logger.LogWarning("Attempted to update non-existent customer ID {CustomerId}", model.PkCustomerId);
-                    return false;
-                }
+                return false;
+            }
 
                 // Update fields (except for EMAIL which is not editable)
-                customer.FirstName = model.FirstName;
-                customer.LastName = model.LastName;
-                customer.PhoneNumber = model.PhoneNumber;
-                customer.DriverLicenceNo = model.DriverLicenceNo;
+            customer.FirstName = model.FirstName;
+            customer.LastName = model.LastName;
+            customer.Email = model.Email;
+            customer.PhoneNumber = model.PhoneNumber;
+            customer.DriverLicenceNo = model.DriverLicenceNo;
 
-                await _db.SaveChangesAsync();
+            await _db.SaveChangesAsync();
 
                 _logger.LogInformation("Profile updated successfully for customer ID {CustomerId}", model.PkCustomerId);
-                return true;
-            }
+            return true;
+        }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error updating profile for customer ID {CustomerId}", model.PkCustomerId);
