c9a1b5b1f54ebf7242205e9527657a0a213cbcab
samankayhanian <skayhanian@my.bcit.ca>
2026-03-13 12:38:50 -0700

    chore: updated profile a-tag

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuxRentals.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
