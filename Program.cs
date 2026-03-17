6d9f6097f7bb29bb82fde694c4127a821671f74a
Poul <poulisbister@gmail.com>
2026-03-17 14:26:38 -0700

    chore: created new repo and added it into program.cs and started modifying the payment service to utilize the repo

--- Program.cs
+++ Program.cs
@@ -48,6 +48,7 @@ builder.Services.AddScoped<RoleRepo>();
 builder.Services.AddScoped<UserRepo>();
 builder.Services.AddScoped<UserRoleRepo>();
 builder.Services.AddScoped<BookingRepo>();
+builder.Services.AddScoped<BookingStatusRepo>();
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 
 // Services
